a5912529330e9b72ea8f7d6d22a78733a8484b1c
LuisMiara <luisgustavomiara@gmail.com>
2018-03-10 15:59:46 -0300

    First Commit

--- src/Players/Monster.java
+++ src/Players/Monster.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package models;
+package Players;
 
 /**
  *
  * @author gustavomiara
  */
-public class heroes {
-    int number;
-    int teste;
+public class Monster {
     
 }
---
+++ src/abstracts/Creature.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstracts;
+
+import interfaces.Generic;
+
+/**
+ *
+ * @author gustavomiara
+ */
+public abstract class Creature implements Generic {
+    private String name;
+    private String classe;
+    private int nivel;
+    private int life;
+    private int magic;
+    private int force;
+    private int agility;
+    private int dexterity;
+    private int intelligence;
+    private int charisma;
+    
+    
+    
+}
