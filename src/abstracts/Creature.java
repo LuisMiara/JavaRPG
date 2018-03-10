21457f9081a123320e7701431ac57a252e562dfa
LuisMiara <luisgustavomiara@gmail.com>
2018-03-10 16:17:08 -0300

    (Feature)Create abstract class Creature

--- src/abstracts/Creature.java
+++ src/abstracts/Creature.java
@@ -12,6 +12,7 @@ import interfaces.Generic;
  * @author gustavomiara
  */
 public abstract class Creature implements Generic {
+        
     private String name;
     private String classe;
     private int nivel;
@@ -22,6 +23,53 @@ public abstract class Creature implements Generic {
     private int dexterity;
     private int intelligence;
     private int charisma;
+
+    public Creature(String name, String classe, int nivel, int life, int magic, int force, int agility, int dexterity, int intelligence, int charisma) {
+        this.name = name;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.life = life;
+        this.magic = magic;
+        this.force = force;
+        this.agility = agility;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+        this.charisma = charisma;
+    }
+    
+    public void atack(){
+    
+    }
+    
+    public void defense(){
+    
+    }
+    
+    public void vivo(){
+    
+    }
+    
+    public void  lostLife(){
+    
+    }
+    
+    public void revitalizar(){
+    
+    }
+    
+    public void descansar(){
+    
+    }
+    
+    @Override
+    public String toString(){
+    
+        return "";
+    }
+    
+
+    
+    
     
     
     
