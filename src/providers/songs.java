6c764bd8f7169bdbb9489a6733a322c393cc774b
LuisMiara <luisgustavomiara@gmail.com>
2018-03-12 21:15:18 -0300

    (Layout)=>Creatand edit screens

---
+++ src/providers/songs.java
@@ -0,0 +1,42 @@
+package providers;
+
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import javax.swing.JOptionPane;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author gustavomiara
+ */
+public class songs {
+
+    static InputStream music;
+    
+
+    
+    static public void startInitialSong(){
+        try {
+            music = new FileInputStream(new File("src/songs/javaRPG.wav"));
+            AudioStream audios = new AudioStream(music);
+            AudioPlayer.player.start(audios);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+            System.out.println("providers.songs.initialSong()" + e.getLocalizedMessage());
+        }
+    }
+    
+    static public void loadGamesSongs(){
+    
+    }
+       
+
+}
