9ad5b4d145c05b1068b21c8b9ac484a759fe1710
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2018-03-20 01:41:50 -0300

    - Added a random method to Creature Class and some others.

---
+++ src/Players/Hero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Players;
+
+import abstracts.Creature;
+
+/**
+ *
+ * @author gustavomiara
+ */
+public class Hero extends Creature{
+    
+    public Hero(String name, String classe, int level, int life, int magic, int force, int agility, int dexterity, int intelligence, int charisma) {
+        super(name, classe, level, life, magic, force, agility, dexterity, intelligence, charisma);
+        
+    }
+
+    
+    
+    
+    
+    
+    
+}
