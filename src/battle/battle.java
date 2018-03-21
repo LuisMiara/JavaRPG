2cf811e4584cd17b07a557a315d6e0a13bcd5728
LuisMiara <luisgustavomiara@gmail.com>
2018-03-21 20:21:04 -0300

    (Feature)=>main class

---
+++ src/battle/battle.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battle;
+
+import Views.Home;
+
+/**
+ *
+ * @author gustavomiara
+ */
+public class battle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+        * here begins all the role
+        */
+        Home home = new Home();
+        home.setVisible(true);
+        
+        
+        
+    }
+    
+}
